3fd5af7d44729350c16936c1ac08500eca5c75b3
Kulani Bamuza <bkulani@gmail.com>
2017-01-20 09:56:12 +0200

    Update healthServiceLocator.php

--- healthServiceLocator/healthServiceLocator.php
+++ healthServiceLocator/healthServiceLocator.php
@@ -1,7 +1,9 @@
 <?php 
 
+/*SIMPLE HEALTH SERVICE LOCATOR USING THE GOOGLE MAPS PLACES AND DISTANCE MATRIX APIS*/
+/*distances in meters*/
+/*time (driving) in minutes*/
 
-	/*SIMPLE HEALTH SERVICE LOCATOR USING THE GOOGLE MAPS PLACES AND DISTANCE MATRIX APIS*/
 	define('PLACES_API_KEY', '');
 	define('DISTANCE_MATRIX_KEY', '');
 	
@@ -10,25 +12,36 @@
     $radius = 5000;
 
 	$foundPlaces = findPlaces($typesOfPlaces, $coordinates, $radius);
-	//foundPlaces is now an array containing associative arrays(formerly JSON objects) with place details
-	//check it out: echo "<pre>".count($foundPlaces)."</pre>";
-	//foundPlaces[i] contains all found locations of each type
-	//echo "<pre>".count($foundPlaces[0])."</pre>";
-	//foundPlaces[i][j] contains associative array with details of a specific place
-	//echo "<pre>".count($placesList[0][0])."</pre>";
+	//echo var_dump($foundPlaces);
 
 	$place_ids = collectPlaceIDs($foundPlaces);
-	$distances = getDistances($place_ids); //distances expressed in meters
+	//echo var_dump($place_ids);
 
+	$id_list = buildIDParameter($place_ids);
+	//echo var_dump($id_list);
+
+	$distanceMatrixResponse = makeRequests($id_list);
+	//echo var_dump($distanceMatrixResponse);
+
+    $destAddresses = getDestAddresses($distanceMatrixResponse);
+    //echo var_dump($destAddresses);
+
+	$distances = getDistances($distanceMatrixResponse);
+	//echo var_dump($distances);
+
+	$durations = getDurations($distanceMatrixResponse);
+	//echo var_dump($durations);
+	
     function findPlaces($typesOfPlaces, $coordinates, $radius)
     {
     	$places = [];
 		foreach ($typesOfPlaces as $type)
 		{
 			$response = file_get_contents("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".$coordinates."&radius=".$radius."&types=".$type."&key=".PLACES_API_KEY);
-	    	$jsonObj = json_decode($response, true); //decode response object
-	    	array_push($places, $jsonObj['results']); //push an associative array of all the locations found
+	    	$jsonObj = json_decode($response, true);
+	    	array_push($places, $jsonObj['results']);
 		}
+		return $places;
 	}
 
 
@@ -40,13 +53,15 @@
 			$id_array = [];
 			foreach ($place as $placeDetails)
 			{ 
-				//echo $placeDetails['types'][0];
 				array_push($id_array, $placeDetails['place_id']);
 			}
-			array_push($place_ids, $id_array);
+			array_push($results, $id_array);
 		}
+		return $results;
 	}
 
+
+	/*concatenate place ids to reduce number of requests made*/
 	function buildIDParameter($IDs)
 	{
 		$list = [];
@@ -55,14 +70,14 @@
 		{
 			for($j = 0; $j < 25; $j++)
 			{
-				if($IDs[$i][$j] != null)
+				if(isset($IDs[$i][$j]))
 				{
-					$string += "place_id:";
-					$string += $IDs[$i][$j];
+					$string .= "place_id:";
+					$string .= $IDs[$i][$j];
 				}
-				if($IDs[$i][$j+1] != null)
+				if(isset($IDs[$i][$j+1]))
 				{
-					$string += "|";
+					$string .= "|";
 				}
 				else
 				{
@@ -70,24 +85,65 @@
 				}
 			}
 			array_push($list, $string);
+			$string = "";
 		}
 		return $list;
 	}
 
-		function getDistances($place_ids)
+	function makeRequests($id_list)
+	{
+		$responses = [];
+		foreach ($id_list as $id)
 		{
-			$id_list = buildIDParameter($place_ids);
-			$distanceArray = [];
-			foreach ($id_list as $id)
-			{
-				$response = file_get_contents('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='.$coordinates.'&destinations=place_id:'.$id.'&mode=walking&key='.DISTANCE_MATRIX_KEY);
-				$responsejson = json_decode($response, true); //decode response object
-				//echo "<pre>".var_dump($responsejson)."</pre>";
-				for ($i=0; $i < count($responsejson['rows'][0]['elements']); $i++)
-				{ 
-					array_push($distanceArray, $responsejson['rows'][0]['elements'][$i]['distance']['value']);
-				}
-				array_push($distances, $distanceArray);
-			}
+			$results = file_get_contents('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins='.$coordinates.'&destinations=place_id:'.$id.'&mode=walking&key='.DISTANCE_MATRIX_KEY);
+			$resultsjson = json_decode($results, true);
+			array_push($responses, $resultsjson);
 		}
- ?>
+		return $responses;
+	}
+
+
+    function getDestAddresses($response)
+    {
+    	$results = [];
+    	foreach ($response as $entry)
+    	{
+    		array_push($results, $entry['destination_addresses']);
+    	}
+    	return $results;
+    }
+
+
+	function getDistances($distanceMatrixResponse)
+	{
+		$results = [];
+		foreach ($distanceMatrixResponse as $entry)
+		{
+			$values = [];
+			foreach($entry['rows'][0]['elements']) as $element)
+			{ 
+				array_push($values, $element['distance']);
+			}
+			array_push($results, $values);
+		}
+		return $results;
+	}
+
+
+
+    function getDurations($distanceMatrixResponse)
+	{
+		$results = [];
+		foreach ($distanceMatrixResponse as $entry)
+		{
+			$values = [];
+			foreach($entry['rows'][0]['elements']) as $element)
+			{ 
+				array_push($values, intval(($element['duration']['value'])/60));
+			}
+			array_push($results, $values);
+		}
+		return $results;
+	}
+
+?>
